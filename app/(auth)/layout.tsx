09b4f93292cd4cfd4578f79931a09772febdc138
Muneef Altamimi <Munef506@gmail.com>
2023-08-28 06:25:37 +0800

    project setup and Clerk authentication added

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Admin Dashboard",
+	description: "Admin Dashboard",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return <main className="flex justify-center items-center">{children}</main>;
+}
